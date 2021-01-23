f91e619eba68b7ef1bddb22f18a803fb63fa1a49
Shifath Salam <shifathsalam@gmail.com>
2021-01-22 20:58:14 -0800

    created post route, model, and controller

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
+
 dotenv.config();
 const app = express();
+
+//routes
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
